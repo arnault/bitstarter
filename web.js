8626def1cf4647ab4678b94dce7ed5c3f26f9252
Ubuntu <ubuntu@ip-10-251-35-130.us-west-2.compute.internal>
2013-07-09 18:46:56 +0000

    hw3 part 1

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+var string = fs.readFileSync("index.html");
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8',string))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
